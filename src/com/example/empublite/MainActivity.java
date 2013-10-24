542950c7c701f189eb4014ab6caca37adaaf2792
Deimantas <deimatracy16@gmail.com>
2013-10-24 18:44:14 +0300

    Build It

---
+++ src/com/example/empublite/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.empublite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnItemSelectedListener {
+
+	TextView selection;
+
+	private static final String[] items = { "lorem", "ipsum", "dolor", "sit",
+			"amet", "consectetuer", "adipiscing", "elit", "morbi", "vel",
+			"ligula", "vitae", "arcu", "aliquet", "mollis", "etiam", "vel",
+			"erat", "placerat", "ante", "porttitor", "sodales", "pellentesque",
+			"augue", "purus" };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		selection = (TextView) findViewById(R.id.selection);
+		Spinner spin = (Spinner) findViewById(R.id.spinner);
+		spin.setOnItemSelectedListener(this);
+
+		ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, items);
+
+		aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin.setAdapter(aa);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		selection.setText(items[arg2]);
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		selection.setText("");
+	}
+
+	
+
+}
